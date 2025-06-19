d06596f61c64d55a4086f7872e6abefdb75bb2b7
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-19 16:21:47 +0530

    Singleton pattern

--- oo-patterns/src/main/java/com/npci/singleton/Application.java
+++ oo-patterns/src/main/java/com/npci/singleton/Application.java
@@ -5,11 +5,27 @@ package com.npci.singleton;
  */
 
 class AppConfig {
-    //....
-    public AppConfig() {
+    private AppConfig() {
         System.out.println("AppConfig created");
     }
+    //public static AppConfig appConfig = new AppConfig(); // Eager Initialization
+    private static AppConfig instance; // = null; // Lazy Initialization
+    public static synchronized AppConfig getInstance() {
+        if (instance == null) {
+            instance = new AppConfig();
+        }
+        return instance;
+    }
 }
+
+// use case of Singleton Pattern
+// 1. Database Connection Pool
+// 2. Logger
+// 3. Configuration Manager
+// 4. Thread Pool
+// 5. Cache Manager
+// 6. Application Context in Spring Framework
+//....
 
 public class Application {
     public static void main(String[] args) {
@@ -17,12 +33,18 @@ public class Application {
         //--------------------------
         // Module-1
         //--------------------------
-        AppConfig appConfig1 = new AppConfig();
+        //AppConfig appConfig1 = new AppConfig();
+        //AppConfig appConfig1 = AppConfig.appConfig;
+        AppConfig appConfig1 = AppConfig.getInstance();
 
         //--------------------------
         // Module-2
         //--------------------------
-        AppConfig appConfig2 = new AppConfig();
+        //AppConfig appConfig2 = new AppConfig();
+        //AppConfig appConfig2 = AppConfig.appConfig;
+        AppConfig appConfig2 = AppConfig.getInstance();
+
+        System.out.println(appConfig1 == appConfig2); // true
 
 
     }
