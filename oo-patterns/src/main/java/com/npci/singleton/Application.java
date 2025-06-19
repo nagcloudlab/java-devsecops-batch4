07570a11ebd4614f4b42fe0107df1dec6c97cba5
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-19 15:56:45 +0530

    Singleton

--- oo-patterns/src/main/java/com/npci/singleton/Application.java
+++ oo-patterns/src/main/java/com/npci/singleton/Application.java
@@ -1,4 +1,29 @@
 package com.npci.singleton;
 
+/**
+ * author: Nikhil
+ */
+
+class AppConfig {
+    //....
+    public AppConfig() {
+        System.out.println("AppConfig created");
+    }
+}
+
 public class Application {
+    public static void main(String[] args) {
+
+        //--------------------------
+        // Module-1
+        //--------------------------
+        AppConfig appConfig1 = new AppConfig();
+
+        //--------------------------
+        // Module-2
+        //--------------------------
+        AppConfig appConfig2 = new AppConfig();
+
+
+    }
 }
