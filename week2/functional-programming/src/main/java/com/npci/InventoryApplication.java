ea44ccf88bd2275790ee47881e680ad697417d59
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-24 16:05:57 +0530

    Why we need FP

--- week2/functional-programming/src/main/java/com/npci/InventoryApplication.java
+++ week2/functional-programming/src/main/java/com/npci/InventoryApplication.java
@@ -1,4 +1,83 @@
 package com.npci;
 
+import com.npci.lib.AppleLibrary;
+import com.npci.model.Apple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class InventoryApplication {
+
+    public static void main(String[] args) {
+
+        List<Apple> inventory = new ArrayList<>();
+        inventory.add(new Apple("red", 150.0));
+        inventory.add(new Apple("green", 120.0));
+        inventory.add(new Apple("green", 130.0));
+        inventory.add(new Apple("red", 160.0));
+
+
+        //-----------------------------
+        // before java 8,  more code to write
+        //-----------------------------
+
+        class RedApplePredicate implements Predicate<Apple> {
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        }
+        class GreenApplePredicate implements Predicate<Apple> {
+            public boolean test(Apple apple) {
+                return "green".equals(apple.getColor());
+            }
+        }
+        class HeavyApplePredicate implements Predicate<Apple> {
+            public boolean test(Apple apple) {
+                return apple.getWeight() > 150;
+            }
+        }
+
+        // Req-1: filter 'green' apples from inventory
+        System.out.println(
+                AppleLibrary.filterApples(inventory, new GreenApplePredicate())
+        );
+
+        // Req-2: filter 'red' apples from inventory
+        System.out.println(
+                AppleLibrary.filterApples(inventory, new RedApplePredicate())
+        );
+
+        // Req-3: filter 'heavy weight' apples from inventory
+        System.out.println(
+                AppleLibrary.filterApples(inventory, new HeavyApplePredicate())
+        );
+
+        System.out.println();
+        System.out.println();
+
+
+        //----------------------------------------------------------------------
+        // From java 8 onwards, we can use lambda expressions aka function,
+        // to make the code more 'concise'
+        //----------------------------------------------------------------------
+
+        // Req-1: filter 'green' apples from inventory
+
+        // (input) ->{}  --> Lambda expression
+        System.out.println(
+                AppleLibrary.filterApples(inventory, (t) -> "green".equals(t.getColor()))
+        );
+        // Req-2: filter 'red' apples from inventory
+        System.out.println(
+                AppleLibrary.filterApples(inventory, (t) -> "red".equals(t.getColor()))
+        );
+        // Req-3: filter 'heavy weight' apples from inventory
+        System.out.println(
+                AppleLibrary.filterApples(inventory, (t) -> t.getWeight() > 150)
+        );
+
+    }
+
 }
+
--- week2/functional-programming/src/main/java/com/npci/lib/AppleLibrary.java
+++ week2/functional-programming/src/main/java/com/npci/lib/AppleLibrary.java
@@ -1,4 +1,62 @@
 package com.npci.lib;
 
-public class AppleLibrary {
+import com.npci.model.Apple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public
+class AppleLibrary {
+
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> obj) {
+        List<Apple> filteredApples = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (obj.test(apple)) {
+                filteredApples.add(apple);
+            }
+        }
+        return filteredApples;
+    }
+
+//    public static List<Apple> filterApplesByWight(List<Apple> inventory, double weight) {
+//        List<Apple> filteredApples = new ArrayList<>();
+//        for (Apple apple : inventory) {
+//            if (apple.getWeight() > weight) {
+//                filteredApples.add(apple);
+//            }
+//        }
+//        return filteredApples;
+//    }
+//
+//    public static List<Apple> filterApplesByColor(List<Apple> inventory, String color) {
+//        List<Apple> filteredApples = new ArrayList<>();
+//        for (Apple apple : inventory) {
+//            if (color.equals(apple.getColor())) {
+//                filteredApples.add(apple);
+//            }
+//        }
+//        return filteredApples;
+//    }
+//
+//    public static List<Apple> filterRedApples(List<Apple> inventory) {
+//        List<Apple> redApples = new ArrayList<>();
+//        for (Apple apple : inventory) {
+//            if ("red".equals(apple.getColor())) {
+//                redApples.add(apple);
+//            }
+//        }
+//        return redApples;
+//    }
+//
+//    public static List<Apple> filterGreenApples(List<Apple> inventory) {
+//        List<Apple> greenApples = new ArrayList<>();
+//        for (Apple apple : inventory) {
+//            if ("green".equals(apple.getColor())) {
+//                greenApples.add(apple);
+//            }
+//        }
+//        return greenApples;
+//    }
 }
+
