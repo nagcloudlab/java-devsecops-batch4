b884fc75e738ddfaebe69f08510fd7ab49c2bc36
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-07-07 09:51:49 +0530

    SonarQube Qulity Gate

--- week3/dev-ops/bin/New/transfer-service-1/src/main/java/org/npci/exception/AccountNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.npci.exception;
-
-public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException() {
-    }
-    public AccountNotFoundException(String message) {
-        super(message);
-    }
-}
--- week3/dev-ops/bin/transfer-service/src/main/java/org/npci/service/TransferService.java
+++
@@ -1,5 +0,0 @@
-package org.npci.service;
-
-public interface TransferService {
-    void initiateTransfer(String fromAccountNumber, String toAccountNumber, double amount);
-}
